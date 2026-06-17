570cf0da27ed66530b1f0459b88cd15d2bde6e91
agm024 <agastyagm2@gmail.com>
2026-06-17 16:33:32 +0530

    Update

--- dropship-showcase/src/main.jsx
+++ dropship-showcase/src/main.jsx
@@ -16,6 +16,27 @@ import { initFirebaseAnalytics } from "./lib/firebase";
 
 void initFirebaseAnalytics();
 
+// Prevent React crashes from third-party DOM mutations (e.g., Google Translate, AdSense Auto Ads)
+if (typeof Node === "function" && Node.prototype) {
+  const originalRemoveChild = Node.prototype.removeChild;
+  Node.prototype.removeChild = function (child) {
+    if (child.parentNode !== this) {
+      console.warn("Prevented React crash: removeChild on non-child node");
+      return child;
+    }
+    return originalRemoveChild.apply(this, arguments);
+  };
+
+  const originalInsertBefore = Node.prototype.insertBefore;
+  Node.prototype.insertBefore = function (newNode, referenceNode) {
+    if (referenceNode && referenceNode.parentNode !== this) {
+      console.warn("Prevented React crash: insertBefore on non-child reference node");
+      return newNode;
+    }
+    return originalInsertBefore.apply(this, arguments);
+  };
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ErrorBoundary>
